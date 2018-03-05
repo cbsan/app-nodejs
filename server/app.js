d2911bbda11632dbb038f894896f06c06ee6fcb4
Cristian BSan <cbsan.dev@gmail.com>
2018-03-05 13:38:15 -0300

    server http

--- server/app.js
+++ server/app.js
@@ -1,30 +1,17 @@
+var http = require('http');
 var fs = require('fs');
-var promise = require('promise');
+
+var server = http.createServer( (request, response) => {
+
+  fs.readFile('index.html', (err, data) => {
+
+    response.write(data);
+  
+    response.end();
+  })
+  //response.writeHead(200, {'Content-type:': 'text/html'});
 
 
-read = (file) => {
-  return new Promise((fulfill, reject) => {
-    fs.readFile(file, (err, data) => {
-      if (err) {
-        reject();
-      } else {
-        fulfill(data.toString());
-      }
-    })
-  });
-}
+});
 
-var file = 'my_file.txt';
-
-read(file)
-    .then((response) => {
-      console.info(response);
-      return '111111';
-    })
-    .then((response) => {
-      console.info(response);
-      return '222222';
-    })
-    .then((response) => {
-      console.info(response);
-    });
+server.listen(3000);
