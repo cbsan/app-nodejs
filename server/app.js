09241acf4ea59c9acd11f32b7f1b42bcf2b569a4
Cristian BSan <cbsan.dev@gmail.com>
2018-03-05 13:19:21 -0300

    promise

--- server/app.js
+++ server/app.js
@@ -1,29 +1,22 @@
 var fs = require('fs');
+var promise = require('promise');
+
+
+read = (file) => {
+  return new Promise((fulfill, reject) => {
+    fs.readFile(file, (err, data) => {
+      if (err) {
+        reject();
+      } else {
+        fulfill(data.toString());
+      }
+    })
+  });
+}
 
 var file = 'my_file.txt';
-// var count = 0;
 
-// console.time('Assinc');
-
-// for (let i = 0; i < 1000; i++) {
-//   fs.readFile('my_file.txt', (err, data) => {
-//     if (err) {
-//       console.error(err);
-//     }
-
-//     count++;
-//     console.log('Assinc', data.toString());
-
-//     if (count === 1000) {
-//         console.timeEnd('Assinc');
-//     }
-    
-//   })
-// }
-//112.473 ms
-console.time('Sync');
-for (let i = 0; i < 1000; i++) {
-    var data = fs.readFileSync('my_file.txt');
-    console.log('Sync', data.toString());
-}
-console.timeEnd('Sync');
+read(file)
+  .then((response) => {
+    console.info(response);
+  });
