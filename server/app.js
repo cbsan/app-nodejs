ea6cf2e650b125054925450d1cb4c4bf9d2ecb4f
Cristian BSan <cbsan.dev@gmail.com>
2018-03-05 13:44:06 -0300

    server

--- server/app.js
+++ server/app.js
@@ -5,11 +5,11 @@ var server = http.createServer( (request, response) => {
 
   fs.readFile('index.html', (err, data) => {
 
+    response.writeHead(200, {'Content-type:': 'text/html'});
     response.write(data);
   
     response.end();
   })
-  //response.writeHead(200, {'Content-type:': 'text/html'});
 
 
 });
