da1e7af15d456e9330702425655993f322dd2a7d
Cristian BSan <cbsan.dev@gmail.com>
2018-03-05 12:53:26 -0300

    Events

--- server/app.js
+++ server/app.js
@@ -1,3 +1,26 @@
-var promise = require('promise');
+var EventEmitter = require('events');
 
-console.info(promise);
+// var emitter = new EventEmitter();
+
+// emitter.on('meu_evento', function (value) {
+//     console.info('meu envento '+value);
+// });
+
+// emitter.emit('meu_evento', 123);
+
+class Cao extends EventEmitter
+{
+    latir() {
+        console.info('Au au!');
+    }
+}
+
+var Rex = new Cao();
+
+// Rex.on('pessoa_no_portao', Rex.latir);
+Rex.once('pessoa_no_portao', Rex.latir);
+
+Rex.emit('pessoa_no_portao');
+Rex.emit('pessoa_no_portao');
+// Rex.removeListener('pessoa_no_portao', Rex.latir);
+Rex.emit('pessoa_no_portao');
