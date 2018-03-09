faab456f5d7a68f2bb56740f389bffbf0a46b3fc
Cristian BSan <cbsan.dev@gmail.com>
2018-03-09 12:37:43 -0300

    view jade

--- server/app.js
+++ server/app.js
@@ -2,12 +2,14 @@ var express = require('express');
 
 var app = express();
 
-app.use(express.static('public'));
+app.use('/static', express.static('public'));
+app.set('views', './views');
+app.set('view engine', 'jade');
 
-app.get('/abc/:id', function (req, resp) {
-  resp.send('Hi!!! '+req.params.id);
+app.get('/', function (req, resp) {
+  resp.render('index');
 });
 
 app.listen(3000, function() {
   console.info('App rodando na porta 3000');
-})
+});
